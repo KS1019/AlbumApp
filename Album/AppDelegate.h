6561799863c589b3046cad37882fefb6b432fac1
KOTARO SUTO <ng1a5ss43@gmail.com>
2015-05-04 14:15:28 +0900

    Initial Commit

---
+++ Album/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Album
+//
+//  Created by Kotaro Suto on 2015/05/04.
+//  Copyright (c) 2015年 Kotaro Suto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Album/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Album
+//
+//  Created by Kotaro Suto on 2015/05/04.
+//  Copyright (c) 2015年 Kotaro Suto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
